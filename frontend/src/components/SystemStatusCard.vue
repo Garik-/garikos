3ff4d7326129e57d4b0c28f1d45d48643fec9775
Gar|k <garik.djan@gmail.com>
2025-03-15 17:20:31 +0300

    feat: system status work

--- frontend/src/components/SystemStatusCard.vue
+++ frontend/src/components/SystemStatusCard.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-import { onMounted, useTemplateRef } from 'vue'
+import { onMounted, onUnmounted, useTemplateRef, ref } from 'vue'
 import ApexCharts from 'apexcharts'
 import { formatBytes } from '@/utils/formatter'
+import { formatter } from '@/utils/formatter'
+import { createSSEConnection } from '@/services/sseService'
+import { SYSTEM_SSE_URL } from '@/config/constants'
+
+const cpuTemperature = ref('')
+const memBytes = ref('')
 
 let cpuChart: ApexCharts | null = null
 const cpuEl = useTemplateRef('cpu-activity')
@@ -56,11 +62,26 @@ const chartOptions = {
   colors: [tabler.getColor('primary')],
 }
 
+let eventSource: EventSource | null = null
+
+type Data = {
+  cpu: [number]
+  sensors: [
+    {
+      temperature: number
+    },
+  ]
+  mem: {
+    used: number
+    usedPercent: number
+  }
+}
+
 onMounted(() => {
   cpuChart = new ApexCharts(
     cpuEl.value,
     Object.assign({}, chartOptions, {
-      series: [35],
+      series: [],
       labels: ['ЦПУ'],
     }),
   )
@@ -68,13 +89,27 @@ onMounted(() => {
   memChart = new ApexCharts(
     memEl.value,
     Object.assign({}, chartOptions, {
-      series: [78],
+      series: [],
       labels: ['ЗУПВ'],
     }),
   )
 
   cpuChart.render()
   memChart.render()
+
+  eventSource = createSSEConnection(SYSTEM_SSE_URL, (d) => {
+    console.log(d)
+
+    cpuTemperature.value = formatter.format((d as Data).sensors[0].temperature)
+    cpuChart?.updateSeries((d as Data).cpu.map((value) => Math.round(value * 100) / 100))
+
+    memChart?.updateSeries([Math.round((d as Data).mem.usedPercent * 100) / 100])
+    memBytes.value = formatBytes((d as Data).mem.used)
+  })
+})
+
+onUnmounted(() => {
+  eventSource?.close()
 })
 </script>
 <template>
@@ -86,11 +121,11 @@ onMounted(() => {
       <div class="row">
         <div class="col-6">
           <div ref="cpu-activity"></div>
-          <h4 class="card-text text-center">26,5°</h4>
+          <h4 class="card-text text-center">{{ cpuTemperature }}°</h4>
         </div>
         <div class="col-6">
           <div ref="mem-activity"></div>
-          <h4 class="card-text text-center">{{ formatBytes(56.25 * 1024 * 1024) }}</h4>
+          <h4 class="card-text text-center">{{ memBytes }}</h4>
         </div>
       </div>
     </div>
--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,1 +1,3 @@
 export const BALCONY_SSE_URL = 'http://raspberrypi.local:8001/subscribe'
+export const SYSTEM_SSE_URL = 'http://raspberrypi.local:8002/system'
+export const DISK_ENDPOINT_URL = 'http://raspberrypi.local:8002/disk'
